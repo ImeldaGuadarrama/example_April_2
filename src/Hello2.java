961478164fb4420fba6d84c49c3fa64cf4696c37
imegua09 <imegua09@gmail.com>
2013-03-25 19:55:33 -0400

    Give a personalized hello.

---
+++ src/Hello2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hello2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter your name");
+		name=input.next();
+		System.out.println("THIS IS A DEBUG LINE!!");
+		//we want to commit this patch without the debug line BUT keeping it in 
+		//my working copy
+		System.out.printf("Hello %s!!", name);
+		
+		
+
+	}
+
+}
