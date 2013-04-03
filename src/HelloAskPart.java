d6d78347effed0392f1d56df9d28a8bc018c5754
imegua09 <imegua09@gmail.com>
2013-04-02 21:39:23 -0400

    add FIRST

--- src/HelloAskPart.java
+++ src/HelloAskPart.java
@@ -4,7 +4,7 @@ public class HelloAskPart {
 	
 	public void askName(StringBuilder first, StringBuilder last){
 		Scanner input= new Scanner(System.in);
-		System.out.println("Enter your name:");
+		System.out.println("Enter your FIRST name:");
 		first.append(input.next());
 		System.out.println("\nNow enter your last name");
 		last.append(input.next());
